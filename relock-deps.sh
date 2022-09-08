10167f06770c46cd19e954240eb9f7d48d286636
Ben Mares <services-git-throwaway1@tensorial.com>
2022-09-08 22:09:10 +0000

    Don't pre-delete conda-lock.yml

--- relock-deps.sh
+++ relock-deps.sh
@@ -5,5 +5,4 @@ set -euf -o pipefail
 # Change to script directory
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-rm -f conda-lock.yml
 conda-lock -f .devcontainer/dev-conda-environment.yaml -f conda-environment.yaml
